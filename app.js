13a687313d2e1fe846d50caeca4a120dbc8e7653
Sakon Plangklang <iamsabell@gmail.com>
2025-09-16 22:38:33 +0700

    Add static file to the page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const postsRouter = require("./routes/postsRoutes");
 const app = express();
 // Parse JSON bodies
 app.use(express.json());
+// Serve static files
+app.use(express.static(`${__dirname}/public`));
 // Use router as sub-app
 app.use("/api/v1/posts", postsRouter);
 
